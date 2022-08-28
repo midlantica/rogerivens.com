6b7407f23d334b59f4500d404e4254a547e6a8a0
Drew Harper <druharper@gmail.com>
2022-08-28 13:52:58 -0500

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), vue()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    vue()
+  ]
 });
---
+++ src/components/slides.vue
@@ -0,0 +1,45 @@
+<template>
+  <vueper-slides
+    fade
+    :infinite="false"
+    :bullets="false"
+    :slide-ratio="1 / 1.3"
+    autoplay
+    fractions
+    progress
+  >
+    <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image">
+      <template v-slot:loader>
+        <i class="icon icon-loader spinning"></i>
+        <span>Loading...</span>
+      </template>
+    </vueper-slide>
+  </vueper-slides>
+</template>
+
+<script setup lang="ts">
+// import { ref } from "vue";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+
+// let title = ref('')
+// const imgPath = ('/public/img/portfolio/01_uiux/')
+let slides = [
+  {
+    image: "/roger-pic.jpg",
+  },
+  {
+    image: "https://picsum.photos/643/929",
+  },
+  {
+    image: "https://picsum.photos/643/929",
+  },
+  {
+    image: "https://picsum.photos/643/929",
+  },
+]
+</script>
+
+<style lang="scss">
+  @import "../../assets/css/carousel.scss";
+</style>
