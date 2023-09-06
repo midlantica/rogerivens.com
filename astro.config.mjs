decbac3fbb7a4486dfb1afacb5738a0e7d811cae
midlantica <druharper@gmail.com>
2023-09-06 13:11:27 -0500

    Broken

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import mdx from "@astrojs/mdx"
+import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    vue()
-  ]
-});
+  integrations: [tailwind(), mdx(), vue()],
+})
