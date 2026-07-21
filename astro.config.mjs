9ca631a0909fdce83e620009f6ebfe95072283df
Drew Harper <druharper@gmail.com>
2026-07-20 22:52:30 -0500

    save deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    mdx(),
     vue()
   ],
   vite: {
